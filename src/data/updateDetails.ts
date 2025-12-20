debb1311034d8da2330479b271b2166658dd9d5b
裕安 蔡 <a0909219938@gmail.com>
2025-12-20 11:37:21 +0800

    修正更新訊息

--- src/data/updateDetails.ts
+++ src/data/updateDetails.ts
@@ -3,9 +3,9 @@ import { updateDetailsItem } from "@/interface/global";
 //最新公告訊息
 let updateDetails:updateDetailsItem = {
     type:"Importer",
-    updateKey:'StarRailApi_20251107',
+    updateKey:'StarRailApi_20251220',
     updateType:'StarRailApiUpdateDetails',
-    updateDate: new Date('2025-11-07'),
+    updateDate: new Date('2025-12-20'),
     updateTitle:"遺器重洗模擬器 崩鐵3.8更新公告",
     updateContent:[
         "新增3.8腳色選項:大理花",
