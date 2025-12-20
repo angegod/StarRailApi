b68b5c9f2c64a5d9045c6721879118e4903545a7
裕安 蔡 <a0909219938@gmail.com>
2025-12-20 11:22:20 +0800

    #add
    新增3.8更新訊息

--- src/data/updateDetails.ts
+++ src/data/updateDetails.ts
@@ -6,11 +6,10 @@ let updateDetails:updateDetailsItem = {
     updateKey:'StarRailApi_20251107',
     updateType:'StarRailApiUpdateDetails',
     updateDate: new Date('2025-11-07'),
-    updateTitle:"遺器重洗模擬器 崩鐵3.7更新公告",
+    updateTitle:"遺器重洗模擬器 崩鐵3.8更新公告",
     updateContent:[
-        "新增3.7腳色選項:昔漣",
-        "優化歷史紀錄排版",
-        "重洗期望機率計算優化"
+        "新增3.8腳色選項:大理花",
+        "細項優化"
     ]
 };
 
